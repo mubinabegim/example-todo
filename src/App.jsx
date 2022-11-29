02fc9325bf1505656c07ba87f91ce2f92e5b1023
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2022-11-29 21:30:07 +0500

    create react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+function App() {
+
+  return (
+    <div className="App">
+      <div className="card border rounded shadow">
+        <div className="card-title border">
+          <p className="text-orange-500 text-5xl">Today's todo</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
