c0f128ace423e3fce5a0b32c9da5ff6977a733e2
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2022-11-29 21:54:42 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react"
+import './styles/App.css'
 function App() {
 
   return (
     <div className="App">
-      <div className="card border rounded shadow">
-        <div className="card-title border">
-          <p className="text-orange-500 text-5xl">Today's todo</p>
+      <div className="card border rounded shadow mx-auto my-10">
+        <div className="card-title border-b-2 border-orange-200">
+          <p className="text-orange-500">Today's todo</p>
+        </div>
+        <div className="card-body divide-y divide-slate-200">
+          <div>nimadir</div>
+          <div>nimadir</div>
+          <div>nimadir</div>
+
         </div>
       </div>
     </div>
