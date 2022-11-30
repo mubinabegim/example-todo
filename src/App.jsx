e35a5c9aaad35d9e3f8e05211be0378d7df026b4
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2022-11-30 23:33:56 +0500

    done array

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import React from "react"
-import './styles/App.css'
+import React from "react";
+import "./styles/App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 function App() {
-
+  let todoList = [
+    { id: 111, title: "write the code" },
+    { id: 121, title: "clean the home" },
+    { id: 131, title: "cook food" },
+  ];
   return (
     <div className="App">
       <div className="card border rounded shadow mx-auto my-10">
-        <div className="card-title border-b-2 border-orange-200">
-          <p className="text-orange-500">Today's todo</p>
+        <div className="card-title border-b-2 border-amber-200">
+          <p className="text-amber-500 p-2 font-semibold">Today's todo</p>
         </div>
         <div className="card-body divide-y divide-slate-200">
-          <div>nimadir</div>
-          <div>nimadir</div>
-          <div>nimadir</div>
-
+          {todoList.map((todo, index) => {
+            return (
+              <div className="flex justify-between items-center my-2 p-2" key={todo.id}>
+                {todo.title}
+                <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
